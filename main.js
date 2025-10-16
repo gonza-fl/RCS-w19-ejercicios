84aff89fa0033bd407fc8bcd0641bdde0d0d74b3
Luciano () <luciano1090@gmail.com>
2025-10-15 21:24:11 -0300

    feat: tarea1

---
+++ main.js
@@ -0,0 +1,30 @@
+//Mostrar mensaje en consola
+console.log("Este es el mensaje de Luciano Caro")
+
+//Variables
+var helloworld = "Hola JavaScript"
+
+let helloworld2 = "Hola de nuevo JS"
+
+const helloworld3 = "Hola otra vez JS"
+
+//declaración de variable: 
+let c 
+//inicializacion de variable
+c = "holajs"
+
+//Operadores
+let a = 10
+let b = 30
+
+
+console.log (a + b)
+console.log (a - b)
+console.log (a * b)
+console.log (a / b)
+
+let variablearitmetica = 5
+variablearitmetica += 2
+console.log(variablearitmetica)
+
+// Comentario que explica: let es una variable despues le damos un nombre 
