f45452aaeb4f719a5b1a659a74cd23bb8695019e
Luciano () <luciano1090@gmail.com>
2025-10-18 19:20:53 -0300

    feat: ejercicio completado

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-//Mostrar mensaje en consola
-console.log("Este es el mensaje de Luciano Caro")
+//Mostrar al menos un mensaje con console.log()
+console.log("Este es el mensaje de Luciano Caro");
 
+//Declarar y asignar variables (let, const, var) (diferenciar declaración y asignación cuando sea posible)
 //Variables
-var helloworld = "Hola JavaScript"
+var helloworld = "Hola JavaScript";
+console.log(helloworld);
 
-let helloworld2 = "Hola de nuevo JS"
+let helloworld2 = "Hola de nuevo JS";
+console.log(helloworld2);
 
-const helloworld3 = "Hola otra vez JS"
+const helloworld3 = "Hola otra vez JS";
+//const helloworld2 = "Esta const no se puede volver a declarar, en la consola da SyntaxError"
+console.log(helloworld3);
 
-//declaración de variable: 
-let c 
-//inicializacion de variable
-c = "holajs"
+//Declaración de variable:
+let c;
+//Inicialización de variable
+c = "Hola JS";
+console.log(c);
 
+//Usar operadores (aritméticos, de asignación, etc.)
 //Operadores
-let a = 10
-let b = 30
+let a = 10;
+let b = 2;
 
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
-console.log (a + b)
-console.log (a - b)
-console.log (a * b)
-console.log (a / b)
+let variablearitmetica = 5;
+variablearitmetica += 2;
+console.log(variablearitmetica);
 
-let variablearitmetica = 5
-variablearitmetica += 2
-console.log(variablearitmetica)
-
-// Comentario que explica: let es una variable despues le damos un nombre 
+/*
+"let" es un tipo de variable a la cual
+le damos un nombre y después se le asigna un valor. Esta valor
+puede ser cambiado mas adelante según sea la necesidad
+ */
