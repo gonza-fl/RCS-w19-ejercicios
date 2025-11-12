44942534b9eae06cde435acb31b3c932a7dcbb5b
Gonza[TFKL] <gonzaloflamenci4@gmail.com>
2025-11-12 19:36:54 -0300

    clase 8

---
+++ clase8-metodos-arreglos/main.js
@@ -0,0 +1,75 @@
+const yerbas = ['la tranquera', 'playadito', 'taragüi', 'cachamate', 'verdeflor', 'nobleza gaucha']
+
+// console.error(yerbas.length)
+// console.log(yerbas.sort)
+// console.clear()
+
+// for (const yerba of yerbas) {
+//   console.log(yerba)
+// }
+
+
+// yerbas.sort()
+// yerbas.reverse()
+
+// for (const yerba of yerbas) {
+//   console.log(yerba)
+// }
+
+// function showData (array, method) {
+//   array[method]();
+//   console.log('********************************');
+//   for (const element of array) {
+//     console.log(element)
+//   }
+// }
+
+// const saludo = 'hola' // ['h','o','l','a']
+
+// console.log(saludo.split(''))
+
+// for (const caracter of saludo) {
+//   console.log(caracter);
+// }
+
+// showData(yerbas, 'sort')
+// showData(yerbas, 'reverse')
+
+// const showDataFnAnonima = function (array, method) {
+//   array[method]();
+//   console.log('********************************');
+//   for (const element of array) {
+//     console.log(element)
+//   }
+// }
+
+// const showDataArrowFunction = (array, method) => {
+//   array[method]();
+//   console.log('********************************');
+//   for (const element of array) {
+//     console.log(element)
+//   }
+// }
+
+// yerbas.forEach(e => {
+//   console.log(e)
+// });
+
+// ((array, method) => {
+//   array[method]();
+//   console.log('********************************');
+//   for (const element of array) {
+//     console.log(element);
+//   }
+// })(yerbas, 'pop'); // IIFE
+
+// yerbas['extra'] = 'Nueva';
+
+// for (let index = 0; index < yerbas.length; index++) {
+//   const yerba = yerbas[index];
+//   console.log(yerba);
+// }
+
+const cervezas = ['quilmes', 'norte', 'heiniken', 'stella artois'];
+
+
